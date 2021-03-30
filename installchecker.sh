dfd0d78c05a7fc069003f17cf9a3435331eff2b5
s-um <yoonms0101@gmail.com>
2021-03-30 22:10:51 +0900

    final...maybe...?

--- installchecker.sh
+++ installchecker.sh
@@ -1,5 +1,4 @@
 mkdir ~/alias_setting
-cat ./installchecker.sh | grep "^#" | tr -d '#' > ~/alias_setting/assignment_check.sh
+cp ./assignment_checker.sh ~/alias_setting/assignment_check.sh
 echo "alias check=\"~/alias_setting/assignment_check.sh\"" >> ~/.zshrc
 source ~/.zshrc
-rm -rf ../42assignment_checker
