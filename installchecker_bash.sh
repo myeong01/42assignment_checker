f5d862c264203a25d56d724100a7b35f2f183219
s-um <yoonms0101@gmail.com>
2021-04-06 11:36:13 +0900

    fix error on mkdir

--- installchecker_bash.sh
+++ installchecker_bash.sh
@@ -1,4 +1,4 @@
-mkdir ~/alias_setting > mkdir_error.txt
+mkdir ~/alias_setting 2> mkdir_error.txt
 rm mkdir_error.txt
 cp ./assignment_checker_bash.sh ~/alias_setting/assignment_check.sh
 echo "alias check=\"~/alias_setting/assignment_check.sh\"" >> ~/.bashrc
